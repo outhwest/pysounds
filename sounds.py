8a248f876ec516819bd9ae02babe2c9cb9f1c480
Cameron Orth <outhwest@gmail.com>
2017-10-31 19:55:32 -0400

    Removed pre-refactor code and handled initial whitespace

--- sounds.py
+++ sounds.py
@@ -1,43 +1,5 @@
-##def Soundex(strInit):
-##    group1 = ('b', 'f', 'p', 'v')
-##    group2 = ('c', 'g', 'j', 'k', 'q', 's', 'x', 'z')
-##    group3 = ('d', 't')
-##    group4 = ('l',)
-##    group5 = ('m', 'n')
-##    group6 = ('r',)
-##    
-##    strFinal = [strInit[0]]
-##    formerValue = 0
-##    curValue = 0
-##    for letter in strInit[1:]:
-##        if letter == ' ':
-##            curValue = '0'
-##        elif letter in group1:
-##            curValue = '1'
-##        elif letter in group2:
-##            curValue = '2'
-##        elif letter in group3:
-##            curValue = 3
-##        elif letter in group4:
-##            curValue = 4
-##        elif letter in group5:
-##            curValue = 5
-##        elif letter in group6:
-##            curValue = 6
-##        if curValue != formerValue:
-##            if curValue != 0:
-##                strFinal.append(curValue)
-##            formerValue = curValue
-##    if len(strFinal) == 1:
-##        strFinal.append(['0','0', '0'])
-##    elif len(strFinal) == 2:
-##        strFinal.append(['0', '0'])
-##    elif len(strFinal) == 3:
-##        strFinal.append('0')
-##
-##    return str(strFinal[:4])
-
 def Soundex(strInit, codeLength=4):
+    
     ignored = "'.?!@hw"
     groups = (
         (' '),
@@ -49,7 +11,7 @@ def Soundex(strInit, codeLength=4):
         ('r',),
         #('a', 'e', 'i', 'o', 'u', 'y')
         )
-    strFinal = [strInit[0]]
+    strFinal = [strInit.lstrip()[0]]
     strInit = strInit.lower()
     formerVal = 7
     curVal = 7
