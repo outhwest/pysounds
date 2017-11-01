d194e3d207b594bdad1ff1352ceceb4a1848272b
Cameron Orth <outhwest@gmail.com>
2017-11-01 10:13:17 -0400

    generalized Soundex, starting on Metaphone

--- sounds.py
+++ sounds.py
@@ -1,4 +1,6 @@
-def Soundex(strInit, codeLength=4):
+from itertools import islice
+
+def Soundex(strInit, codeLength=4, start=False):
     
     ignored = "'.?!@hw"
     groups = (
@@ -11,14 +13,21 @@ def Soundex(strInit, codeLength=4):
         ('r',),
         #('a', 'e', 'i', 'o', 'u', 'y')
         )
-    strFinal = [strInit.lstrip()[0]]
+    strInit = strInit.lstrip()
+    if not start:
+        strFinal = [strInit[0]]
     strInit = strInit.lower()
     formerVal = 7
     curVal = 7
+    inGroups = False
     for idx, group in enumerate(groups):
         if strInit[0] in group:
             formerVal = idx
-    
+            inGroups = True
+            if start:
+                strFinal = [str(idx)]
+    if start and not inGroups:
+        strFinal = ['A']
     inGroups = False
     strInit = ''.join(c for c in strInit if c not in ignored)
     for letter in strInit[1:]:
@@ -39,4 +48,72 @@ def Soundex(strInit, codeLength=4):
     if strLen < codeLength:
         strFinal.extend(['0']*codeLength)
 
-    return ''.join(strFinal[:4]) 
+    return ''.join(strFinal[:4])
+
+def getNGrams(source, n):
+    for i in range(len(source)- n + 1):
+        yield ''.join(islice(source, i, i + n))
+
+def Metaphone(strInit, initialVowel=True):
+    strInit = strInit.upper()
+    twoGrams = getNGrams(strInit, 2)
+    threeGrams = getNGrams(strInit, 3)
+    vowels = "AEIOU"
+    #previous and current 2Grams
+    twos = [None, next(twoGrams)]
+    #previous two and current 3Grams
+    threes = [None, None, next(threeGrams)]
+
+    #Deal with the first letter logic
+    initialDict = {
+        'KN': 'N',
+        'GN': 'N',
+        'PN': 'N',
+        'AE': 'E',
+        'WR': 'R'
+        }
+    
+    if twos[1] in initialDict:
+        finalList = [initialDict[twos[1]]]
+        twos[0] = twos[1]
+        try:
+            twos[1] = next(twoGrams)
+        except:
+            twos[1] = None
+        threes[1] = threes[2]
+        try:
+            threes[2] = next(threeGrams)
+        except:
+            threes[2] = None
+    else:
+        if initialVowel or (strInit[0] not in vowels):
+            finalList = [strInit[0]]
+        #ie initialVowel=False and first letter is a value
+        else:
+            finalList = ['A']
+
+    #after special casing first letter, rest of word is the same
+    for c in strInit[1:]:
+        #increment the previous ngrams lists so that c is in each ngram
+        twos[0] = twos[1]
+        threes[0] = threes[1]
+        threes[1] = threes[2]
+        try:
+            twos[1] = next(twoGrams)
+        except:
+            twos[1] = None
+        try:
+            threes[2] = next(threeGrams)
+        except:
+            threes[2] = None
+        
+        # Check for duplicate adjacent letters and skip when at the second
+        if c != 'C' and twos[0] == c*2:
+            continue
+        if c == 'G':
+            pass
+
+    return ''.join(finalList)
+
+def isHomophone(a,b, algorithm=Soundex):
+    return algorithm(a) == algorithm(b)
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-from sounds import Soundex
+from sounds import Soundex, Metaphone, isHomophone
+from functools import partial
+
+soundFromStart = partial(Soundex, start=True)
 
 a = "Robert"
 b = "Egbert"
@@ -6,10 +9,18 @@ c = "Rupert"
 d = "Ashcraft"
 e = "Ashcroft"
 f = "Tymczak"
+g = "fat"
+h = "phat"
 
-assert Soundex(a) == Soundex(c)
-assert Soundex(a) != Soundex(b)
-assert Soundex(b) != Soundex(c)
-assert Soundex(d) == Soundex(e)
+assert isHomophone(a,c)
+assert not isHomophone(a,b)
+assert not isHomophone(b,c)
+assert isHomophone(d,e)
 assert Soundex(d) == "A261"
 assert Soundex(f) == "T522"
+assert not isHomophone(g,h)
+assert isHomophone(g,h,algorithm=soundFromStart)
+
+assert isHomophone(d,e, algorithm=Metaphone)
+assert isHomophone(a,b, algorithm=Metaphone)
+assert isHomophone(g,h, algorithm=Metaphone)
